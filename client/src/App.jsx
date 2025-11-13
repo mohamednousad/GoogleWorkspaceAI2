07d48d21754fc6580519d54c4dbc7cb9db179e84
Mohamed Nousad <119624830+mohamednousad@users.noreply.github.com>
2025-11-13 13:57:19 +0530

    Delete client directory

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./components/Dashboard";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Dashboard />
-    </div>
-  );
-}
